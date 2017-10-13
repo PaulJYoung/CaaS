92133b0e52e42eb20c40a57879ac91d3367791b7
root <root@ip-172-31-2-149.eu-west-2.compute.internal>
2017-10-13 14:30:57 +0000

    Sync up remote and master

--- CaaS/settings.py
+++ CaaS/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'CaaS',
 	'USER': 'root',
-	'PASSWORD': '',
+	'PASSWORD': 'ay65112025',
         'HOST': 'db',
         'PORT': '3306'
     }
