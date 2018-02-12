21f93a7e70b09f6df2c57258a14ba18a294dfbc7
Paul Young <31133967+PaulJYoung@users.noreply.github.com>
2018-02-12 15:14:43 +0000

    Update views.py

--- CloudasaService/views.py
+++ CloudasaService/views.py
@@ -5,7 +5,7 @@ from django.core.urlresolvers import reverse
 from django.views import generic
 from django.views.generic import DetailView
 from django.views.generic import ListView
-from .models import Material
+from .models import Material Aws
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.db.models import F
