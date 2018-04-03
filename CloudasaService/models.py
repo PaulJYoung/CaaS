99d6c9cc9476afaac47c8665bcdecbbcaa0fe050
Paul Young <31133967+PaulJYoung@users.noreply.github.com>
2018-04-03 18:14:17 +0100

    Update models.py

--- CloudasaService/models.py
+++ CloudasaService/models.py
@@ -37,3 +37,11 @@ class Rackspace(models.Model):
 	title_date = models.DateTimeField('date added')
 	def __str__(self):
         	return self.title
+
+class Contact(models.Model):
+	firstname = models.CharField(max_length=50)
+	surname = models.CharField(max_length=50)
+	emailaddr = models.EmailField(max_length=100)
+	comment = models.TextField(null=True, max_length=2000)
+	def __str__(self):
+        	return self.title
