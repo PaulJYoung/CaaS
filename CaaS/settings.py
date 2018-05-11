df5af0e44d65b9aa9cbdd9db0460ffa545dcfe5f
Paul Young <paul@cosolutions.co.uk>
2018-05-11 16:11:50 +0100

    Update settings.py

--- CaaS/settings.py
+++ CaaS/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'CaaS',
 	'USER': 'root',
-	'PASSWORD': '',
+	'PASSWORD': 'ay65112025',
         'HOST': 'db',
         'PORT': '3306'
     }
