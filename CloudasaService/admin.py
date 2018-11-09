19bd2bc16a279a00922de628fc447632d40aedaf
Paul Young <paul@cosolutions.co.uk>
2018-11-09 15:05:28 +0000

    Update admin.py

--- CloudasaService/admin.py
+++ CloudasaService/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
 # from .models import Material
-from .models import Material, AWS, Azure, Google, ContactUs, Lists
+from .models import Material, AWS, Azure, Google, ContactUs, Lists, Updates
 
-@admin.register(Material, AWS, Azure, Google, ContactUs, Lists)
+@admin.register(Material, AWS, Azure, Google, ContactUs, Lists, Updates)
 
 class DefaultAdmin(admin.ModelAdmin):
 	pass
