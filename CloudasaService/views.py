70caa4ac57f19a774abec256a1c3508ee40b3190
PaulJYoung <paul@cosolutions.co.uk>
2018-11-13 18:52:44 +0000

    Update views for contact form changes

--- CloudasaService/views.py
+++ CloudasaService/views.py
@@ -119,14 +119,15 @@ def contactview(request):
         if form.is_valid():
             messages.success(request, 'Message saved, thank you!')
             v_firstname = form.cleaned_data['firstname']
-            v_surname = form.cleaned_data['surname']
+#           v_surname = form.cleaned_data['surname']
             v_emailaddr = form.cleaned_data['emailaddr']
             v_comment = form.cleaned_data['post']
             v_comment_date = timezone.now()
-            p = ContactUs.objects.create(firstname=v_firstname, surname=v_surname, emailaddr=v_emailaddr, comment=v_comment)
+            p = ContactUs.objects.create(firstname=v_firstname, emailaddr=v_emailaddr, comment=v_comment) 
+#           p = ContactUs.objects.create(firstname=v_firstname, surname=v_surname, emailaddr=v_emailaddr, comment=v_comment)
 #           return render(request, 'CloudasaService/contact.html')
-	    return HttpResponseRedirect('/CaaS/')
-#           return self.render_to_response(request, 'CloudasaService/contact.html', {'success':success})
+            return HttpResponseRedirect('/CaaS/')
+#       return self.render_to_response(request, 'CloudasaService/contact.html', {'success':success})
     else:
             form = ContactForm()
     return render(request, 'CloudasaService/contact.html', {'form': form})
