753541b798b79538f1c6874d427b9ba4b86a4b27
PaulJYoung <paul@cosolutions.co.uk>
2018-11-13 18:37:20 +0000

    Change contact form

--- CloudasaService/forms.py
+++ CloudasaService/forms.py
@@ -4,8 +4,8 @@ from django.forms import ModelForm, TextInput
 from django.contrib.auth.models import User
 
 class ContactForm(forms.Form):
-    firstname = forms.CharField(label='First Name',widget=forms.Textarea(attrs={'class': 'formlblbox', 'cols': 100, 'rows': 1}),max_length=100)
-    surname = forms.CharField(label='Surname',widget=forms.Textarea(attrs={'class': 'formlblbox', 'cols': 100, 'rows': 1}),max_length=100)
-    emailaddr = forms.CharField(label='Email Addr',widget=forms.Textarea(attrs={'class': 'formlblbox', 'cols': 100, 'rows': 1}),max_length=100)
+    firstname = forms.CharField(label='Name',widget=forms.Textarea(attrs={'class': 'formlblbox', 'cols': 100, 'rows': 1}),max_length=100)
+#   surname = forms.CharField(label='Surname',widget=forms.Textarea(attrs={'class': 'formlblbox', 'cols': 100, 'rows': 1}),max_length=100)
+    emailaddr = forms.CharField(label='Email',widget=forms.Textarea(attrs={'class': 'formlblbox', 'cols': 100, 'rows': 1}),max_length=100)
     post = forms.CharField(label='Message',widget=forms.Textarea(attrs={'class': 'formtxtbox', 'cols': 100, 'rows': 20}), max_length=2000)
     
