f2d89b076d27a6f2ba48e5cbc21a686d65068465
Paul Young <paul@cosolutions.co.uk>
2018-11-19 11:50:19 +0000

    Update models.py

--- CloudasaService/models.py
+++ CloudasaService/models.py
@@ -73,3 +73,9 @@ class Updates(models.Model):
 	title_date = models.DateTimeField('updated')
 	def __str__(self):
        	        return self.title
+
+class UpdateText(models.Model):
+        update_text = models.TextField(null=True, max_length=150)
+	title_date = models.DateTimeField('updated')
+	def __str__(self):
+       	        return self.title
