460a1c0a9dd423b666e6a506bf5197b1ff150ee7
Paul Young <paul@cosolutions.co.uk>
2018-11-20 10:50:58 +0000

    Update base_extras.py

--- CloudasaService/templatetags/base_extras.py
+++ CloudasaService/templatetags/base_extras.py
@@ -6,7 +6,7 @@ register = template.Library()
 @register.simple_tag
 def update_string_header():
     Updatetext = UpdateText.objects.order_by('-post_date')[:1]
-#    context = {
-# 	      'Updatetext': Updatetext,
-#    }
-    return Updatetext
+    context = {
+ 	      'Updatetext': Updatetext,
+    }
+    return context
