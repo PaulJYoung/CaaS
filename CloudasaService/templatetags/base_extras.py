c007cbc8dbf8a72206e5bdb3412ee95d8f8b8103
Paul Young <paul@cosolutions.co.uk>
2018-11-20 09:42:19 +0000

    Update base_extras.py

--- CloudasaService/templatetags/base_extras.py
+++ CloudasaService/templatetags/base_extras.py
@@ -3,11 +3,11 @@ from django .models import UpdateText
 
 register = template.Library()
 
-@register.simple_tag
-def update_string_header(update_value)
-
-    Updatetext = UpdateText.objects.order_by('-post_date')[:1]
-    context = {
- 	      'Updatetext': Updatetext,
-    }
-    return Updatetext
+#@register.simple_tag
+#def update_string_header(update_value)
+#
+#    Updatetext = UpdateText.objects.order_by('-post_date')[:1]
+#    context = {
+# 	      'Updatetext': Updatetext,
+#    }
+#    return Updatetext
