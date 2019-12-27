8d5731a2bb49d97cc6c0b22ac6a6321fcbaa3193
PaulJYoung <paul@cosolutions.co.uk>
2019-12-27 18:39:32 +0000

    model change

--- CloudasaService/models.py
+++ CloudasaService/models.py
@@ -2,14 +2,14 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django_mysql.models import ListCharField
-
-
+#
+#
 class Material(models.Model):
-	title = models.CharField(max_length=50)
- 	title_content = models.TextField(null=True, max_length=6000)
-	title_date = models.DateTimeField('date added')
-	def __str__(self):
-        	return self.title
+    title = models.CharField(max_length=50)
+    title_content = models.TextField(null=True, max_length=6000)
+    title_date = models.DateTimeField('date added')
+    def __str__(self):
+            return self.title
 
 class AWS(models.Model):
 	title = models.CharField(max_length=50)
