33f3c6a56935c292835ae32404213c25378ce8bc
PaulJYoung <paul@cosolutions.co.uk>
2019-12-27 18:54:05 +0000

    change to django.urls after upgrade

--- CloudasaService/views.py
+++ CloudasaService/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from django.http import HttpResponseRedirect
 from django.template import loader
-from django.core.urlresolvers import reverse
+#from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.views import generic
 from django.views.generic import DetailView
 from django.views.generic import ListView
