46f776609de470d19f32a3b01761b2305165f2a3
PaulJYoung <paul@cosolutions.co.uk>
2023-06-23 13:15:31 +0100

    change to models

--- CloudasaService/models.py
+++ CloudasaService/models.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django_mysql.models import ListCharField
+#from django_mysql.models import ListCharField
 #
 #
 class Material(models.Model):
