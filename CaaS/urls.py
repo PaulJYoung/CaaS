9ea6c7af111390106d77f5a107471a80862bac45
Paul Young <paul@cosolutions.co.uk>
2024-12-30 19:20:53 +0000

    CaaS URL change

--- CaaS/urls.py
+++ CaaS/urls.py
@@ -17,7 +17,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'', include('CloudasaService.urls')),
+#   url(r'', include('CloudasaService.urls')),
+    url(r'', include('CaaS.urls')),
 #   url(r'^CaaS/', include('CloudasaService.urls')),
     url(r'^admin/', admin.site.urls),
 ]
